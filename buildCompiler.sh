89c406226ecdeaa28a83b7590777c2322af3a5d6
MacsedProtoss <yu410420@gmail.com>
2020-11-24 16:31:05 +0800

    updated shell

--- buildCompiler.sh
+++ buildCompiler.sh
@@ -1,2 +1,14 @@
-cd ./src && flex lex.l && bison -d -x parser.y
-cd .. && g++ -g ./src/ast.cc ./src/lex.yy.c ./src/parser.tab.c ./src/main.cc ./src/analyzer.cc ./src/functions.cc ./src/variables.cc ./src/ir.cc -I/usr/lib/llvm-10/include -L/usr/lib/llvm-10/lib -lLLVM-10 -std=c++2a -o ./
+echo "compiler build start"
+path=`pwd`
+cd $path/src
+echo "running flex[1/3]"
+flex lex.l
+echo "done"
+echo "running bison[2/3]"
+bison -d -x parser.y
+echo "done"
+cd $path
+echo "running g++[3/3]"
+g++ -g $path/src/ast.cc $path/src/lex.yy.c $path/src/parser.tab.c $path/src/main.cc $path/src/analyzer.cc $path/src/functions.cc $path/src/variables.cc $path/src/ir.cc -I/usr/lib/llvm-10/include -L/usr/lib/llvm-10/lib -lLLVM-10 -std=c++2a -o $path/compiler
+echo "done"
+echo "compiler build finshed"
--- runCompiler.sh
+++ runCompiler.sh
@@ -1,5 +1,12 @@
-./compiler ./test/test.swiftl >> ./test/test.ir
-
-llc -march=x86-64 ./test/test.ir --filetype=obj -o ./test/test.o
-
-clang -v ./test/test.o -o ./test/test
+echo "compiler run start"
+path=`pwd`
+echo "generating ir[1/3]"
+$path/compiler $path/test/test.swiftl >> $path/test/test.ir
+echo "done"
+echo "llc compiling[2/3]"
+llc -march=x86-64 $path/test/test.ir --filetype=obj -o $path/test/test.o
+echo "done"
+echo "clang linking[3/3]"
+clang -v $path/test/test.o -o $path/test/test
+echo "done"
+echo "compiler run finished"
